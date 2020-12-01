65072173048292dffc024854ed5f0baa58453f7d
savirafatika <savira.fatika@students.amikom.ac.id>
2020-12-01 22:50:54 +0700

    add sign up modal with js validation

--- script.js
+++ script.js
@@ -108,3 +108,90 @@ new Typed('#typed',{
    delaySpeed : 100,
    loop : true
 })
+
+// modal item
+const modal = document.getElementById('email-modal')
+const openBtn = document.querySelector('#signup')
+const closeBtn = document.querySelector('.close-btn')
+
+// click event modal
+openBtn.addEventListener('click', () => {
+   modal.style.display = 'block'
+})
+
+closeBtn.addEventListener('click', () => {
+   modal.style.display = 'none'
+})
+
+window.addEventListener('click', (e) => {
+   if (e.target === modal) {
+      modal.style.display = 'none'
+   }
+})
+
+// Form Validation
+const form = document.getElementById('form')
+const name = document.getElementById('name')
+const email = document.getElementById('email')
+const password = document.getElementById('password')
+const passwordConfirm = document.getElementById('password-confirm')
+
+// show error message
+function showError(input, message) {
+   const formValidation = input.parentElement
+   formValidation.className = 'form-validation error'
+
+   const errorMessage = formValidation.querySelector('p')
+   errorMessage.innerText = message
+}
+
+// show valid message
+function showValid(input) {
+   const formValidation = input.parentElement
+   formValidation.className = 'form-validation valid' 
+}
+
+// Check required field 
+function checkRequired(inputArr) {
+   inputArr.forEach(function (input) {
+      if (input.value.trim() === '') {
+         showError(input, `${getFieldName(input)} is required`)
+      } else {
+         showValid(input)
+      }
+   })
+}
+
+// Check input length
+function checkLength(input, min, max) {
+   if (input.value.length < min) {
+      showError(input, `${getFieldName(input)} must be at least ${min} characters`)
+   } else if (input.value.length > max) {
+      showError(input, `${getFieldName(input)} must be less than ${max} characters`)
+   } else {
+      showValid(input)
+   }
+}
+
+// check password match
+function passwordMatch(input1, input2 ) {
+   if (input1.value !== input2.value) {
+      showError(input2, 'Password do not match')
+   }
+}
+
+// get field name
+function getFieldName(input) {
+   return input.name.charAt(0).toUpperCase() + input.name.slice(1)
+}
+
+// Event Lisreners
+form.addEventListener('submit', (e) => {
+   e.preventDefault()
+
+   checkRequired([name, email, password, passwordConfirm])
+   checkLength(name, 3, 30)
+   checkLength(password, 8, 25)
+   checkLength(passwordConfirm, 8, 25)
+   passwordMatch(password, passwordConfirm)
+})
